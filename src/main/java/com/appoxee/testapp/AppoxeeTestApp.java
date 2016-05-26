847173a6acb85cb01882d481d3577ced6b7a289b
yonigross <gross.yoni@gmail.com>
2016-05-26 12:31:40 +0300

    fixed registration problems

--- src/main/java/com/appoxee/testapp/AppoxeeTestApp.java
+++ src/main/java/com/appoxee/testapp/AppoxeeTestApp.java
@@ -3,11 +3,14 @@ package com.appoxee.testapp;
 import android.app.Application;
 
 import com.appoxee.Appoxee;
+import com.appoxee.AppoxeeOptions;
 
 public class AppoxeeTestApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Appoxee.engage(this);
+        AppoxeeOptions opt = new AppoxeeOptions();
+        opt.sdkKey = "574597825dcf97.14767101";
+        Appoxee.engage(this, opt);
     }
 }
