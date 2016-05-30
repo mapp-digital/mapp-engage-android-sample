aa966100af8c2d90e1c4719b344d3ddae2cffb0d
yonigross <gross.yoni@gmail.com>
2016-05-30 20:21:55 +0300

    added google playu services 9.0.0

--- src/main/java/com/appoxee/testapp/AppoxeeTestApp.java
+++ src/main/java/com/appoxee/testapp/AppoxeeTestApp.java
@@ -1,16 +1,45 @@
 package com.appoxee.testapp;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.appoxee.Appoxee;
 import com.appoxee.AppoxeeOptions;
+import com.appoxee.DeviceInfo;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.IOException;
 
 public class AppoxeeTestApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        long start = System.currentTimeMillis();
         AppoxeeOptions opt = new AppoxeeOptions();
         opt.sdkKey = "574597825dcf97.14767101";
         Appoxee.engage(this, opt);
+        long end = System.currentTimeMillis();
+        DeviceInfo info = Appoxee.instance().getDeviceInfo();
+        long total = end - start;
+        Log.i("APX", "Start Service took " + total + " ms on main thread");
+        Log.d("APX", "info (before init finished): " + info);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String token = null;
+                try {
+                    InstanceID instanceID = InstanceID.getInstance(AppoxeeTestApp.this);//.getToken("firebasetest1", "GCM");
+                    token = instanceID.getToken("fir-test1-e10ee",
+                            GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Log.i("APX", "token: " + token);
+            }
+        }).start();
+
+
     }
 }
--- src/main/java/com/appoxee/testapp/MainActivity.java
+++ src/main/java/com/appoxee/testapp/MainActivity.java
@@ -2,6 +2,12 @@ package com.appoxee.testapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.appoxee.Appoxee;
+import com.appoxee.DeviceInfo;
+import com.google.gson.Gson;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +15,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        findViewById(R.id.device_info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DeviceInfo info = Appoxee.instance().getDeviceInfo();
+                Log.d("APX", "info: (click)" + new Gson().toJson(info));
+
+            }
+        });
     }
+
+
 }
