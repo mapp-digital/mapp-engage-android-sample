016cd45f15d8de55b2b369e8b84b2a4b4bfbfa9a
alexeykrichun <alexey.krichun@teradata.com>
2016-07-07 17:54:09 +0300

    Push - custom notification example

--- src/main/java/com/appoxee/testapp/AppoxeeTestApp.java
+++ src/main/java/com/appoxee/testapp/AppoxeeTestApp.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.appoxee.Appoxee;
 import com.appoxee.AppoxeeOptions;
 import com.appoxee.DeviceInfo;
+import com.appoxee.push.CustomXmlLayoutNotificationCreator;
 
 public class AppoxeeTestApp extends Application {
 
@@ -20,13 +21,20 @@ public class AppoxeeTestApp extends Application {
 
         opt.googleProjectId = "94866074595";
 
+        CustomXmlLayoutNotificationCreator.Builder builder = new CustomXmlLayoutNotificationCreator.Builder(this);
+        builder.setLayoutResource(R.layout.custom_notification_layout)
+                .setIconResourceId(R.id.appoxee_default_push_icon)
+                .setTextResourceId(R.id.appoxee_default_push_message)
+                .setTitleResourceId(R.id.appoxee_default_push_subject)
+                .setTimeResourceId(R.id.appoxee_default_push_hour);
+
+        opt.customNotificationCreator = new CustomXmlLayoutNotificationCreator(builder);
+
         Appoxee.engage(this, opt);
         long end = System.currentTimeMillis();
         DeviceInfo info = Appoxee.instance().getDeviceInfo();
         long total = end - start;
         Log.i("APX", "Start Service took " + total + " ms on main thread");
         Log.d("APX", "info (before init finished): " + info);
-
     }
-
 }
