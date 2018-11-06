0d820cadd96c80a1d15faf2d3736f9c81496fe8b
aleksandar.marinkovi <aleksandar.marinkovic@mapp.com>
2018-11-06 15:55:22 +0100

    Code refactoring

--- src/main/java/com/appoxee/testapp/ConfigurationMappOptionsActivity.java
+++ src/main/java/com/appoxee/testapp/ConfigurationMappOptionsActivity.java
@@ -21,7 +21,6 @@ import com.pixplicity.easyprefs.library.Prefs;
 public class ConfigurationMappOptionsActivity extends AppCompatActivity {
 
     private Context appContext;
-
     private AppoxeeOptions appoxeeOptions;
     private EditText etxtSetSdkKey;
     private EditText etxtSetGoogleProjectId;
@@ -36,7 +35,14 @@ public class ConfigurationMappOptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration_mapp_options);
+        init();
 
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        );
+    }
+
+    private void init() {
         appoxeeOptions = ((AppoxeeTestApp) getApplication()).getAppoxeeOptions();
 
         etxtSetSdkKey = findViewById(R.id.etxt_set_sdk_key);
@@ -46,47 +52,46 @@ public class ConfigurationMappOptionsActivity extends AppCompatActivity {
         etxtSetTenantId = findViewById(R.id.etxt_set_tenant_id);
         btnConfigureAppoxeeOptions = findViewById(R.id.btn_configure_appoxee_options);
         btnRefresh = findViewById(R.id.btn_refresh);
-
-        btnConfigureAppoxeeOptions.setOnClickListener(new View.OnClickListener() {
+        btnRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String sdkKey = etxtSetSdkKey.getText().toString();
-                String googleProjectId = etxtSetGoogleProjectId.getText().toString();
-                String cepUrl = etxtSetCepUrl.getText().toString();
-                String appId = etxtSetAppId.getText().toString();
-                String tenantId = etxtSetTenantId.getText().toString();
-
-
-                if (sdkKey.equals("") || googleProjectId.equals("") || cepUrl.equals("") || appId.equals("") || tenantId.equals("")) {
-                    Toast.makeText(ConfigurationMappOptionsActivity.this, "Please fill all field", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                Prefs.putString("SDK_KEY", sdkKey);
-                Prefs.putString("GOOGLE_PROJECT_ID", googleProjectId);
-                Prefs.putString("CEP_URL", cepUrl);
-                Prefs.putString("APP_ID", appId);
-                Prefs.putString("TENANT_ID", tenantId);
-
-                appoxeeOptions.sdkKey = sdkKey;
-                appoxeeOptions.googleProjectId = googleProjectId;
-                appoxeeOptions.cepURL = cepUrl;
-                appoxeeOptions.appID = appId;
-                appoxeeOptions.tenantID = tenantId;
-
-                etxtSetSdkKey.setText("");
-                etxtSetGoogleProjectId.setText("");
-                etxtSetCepUrl.setText("");
-                etxtSetAppId.setText("");
-                etxtSetTenantId.setText("");
-
+                reset();
             }
         });
 
 
-        getWindow().setSoftInputMode(
-                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
-        );
+    }
+
+    private void reset() {
+        String sdkKey = etxtSetSdkKey.getText().toString();
+        String googleProjectId = etxtSetGoogleProjectId.getText().toString();
+        String cepUrl = etxtSetCepUrl.getText().toString();
+        String appId = etxtSetAppId.getText().toString();
+        String tenantId = etxtSetTenantId.getText().toString();
+
+
+        if (sdkKey.equals("") || googleProjectId.equals("") || cepUrl.equals("") || appId.equals("") || tenantId.equals("")) {
+            Toast.makeText(ConfigurationMappOptionsActivity.this, "Please fill all field", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Prefs.putString("SDK_KEY", sdkKey);
+        Prefs.putString("GOOGLE_PROJECT_ID", googleProjectId);
+        Prefs.putString("CEP_URL", cepUrl);
+        Prefs.putString("APP_ID", appId);
+        Prefs.putString("TENANT_ID", tenantId);
+
+        appoxeeOptions.sdkKey = sdkKey;
+        appoxeeOptions.googleProjectId = googleProjectId;
+        appoxeeOptions.cepURL = cepUrl;
+        appoxeeOptions.appID = appId;
+        appoxeeOptions.tenantID = tenantId;
+
+        etxtSetSdkKey.setText("");
+        etxtSetGoogleProjectId.setText("");
+        etxtSetCepUrl.setText("");
+        etxtSetAppId.setText("");
+        etxtSetTenantId.setText("");
     }
 
     public void getConfiguration(View view) {
@@ -123,15 +128,15 @@ public class ConfigurationMappOptionsActivity extends AppCompatActivity {
         appoxeeOptions.appID = getResources().getString(R.string.hint_app_id);
         appoxeeOptions.tenantID = getResources().getString(R.string.hint_tenant_id);
 
-        String str = new StringBuilder("sdkKey: ").append(appoxeeOptions.sdkKey)
-                .append("\n")
-                .append("googleProjectId: ").append(appoxeeOptions.googleProjectId)
-                .append("\n")
-                .append("cepURL: ").append(appoxeeOptions.cepURL)
-                .append("\n")
-                .append("appID: ").append(appoxeeOptions.appID)
-                .append("\n")
-                .append("tenantID: ").append(appoxeeOptions.tenantID).toString();
+        String str = "sdkKey: " + appoxeeOptions.sdkKey +
+                "\n" +
+                "googleProjectId: " + appoxeeOptions.googleProjectId +
+                "\n" +
+                "cepURL: " + appoxeeOptions.cepURL +
+                "\n" +
+                "appID: " + appoxeeOptions.appID +
+                "\n" +
+                "tenantID: " + appoxeeOptions.tenantID;
 
         Toast.makeText(this, str, Toast.LENGTH_LONG).show();
 
@@ -139,6 +144,8 @@ public class ConfigurationMappOptionsActivity extends AppCompatActivity {
 
     public static void hideKeyboardFrom(Context context, View view) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
