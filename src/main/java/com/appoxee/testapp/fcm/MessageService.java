c62cc368251f7c9f65bb8ec2aed82e054cc36b31
aleksandar.marinkovi <aleksandar.marinkovic@mapp.com>
2021-03-08 11:42:20 +0100

    Improve some logic for inapp

--- src/main/java/com/appoxee/testapp/fcm/MessageService.java
+++ src/main/java/com/appoxee/testapp/fcm/MessageService.java
@@ -1,16 +1,20 @@
-//package com.appoxee.testapp.fcm;
-//
-//
-//
-//import com.appoxee.push.PushData;
-//import com.appoxee.push.fcm.MappMessagingService;
-//import com.google.firebase.messaging.RemoteMessage;
-//public  class MessageService extends MappMessagingService {
-//
-//    @Override
-//    public void onMessageReceived(RemoteMessage remoteMessage) {
-//        super.onMessageReceived(remoteMessage);
-//        //TODO process push data if you want
-//        PushData pushData = getData(remoteMessage);
-//    }
-//}
+package com.appoxee.testapp.fcm;
+
+
+
+
+import com.appoxee.push.fcm.MappMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+public  class MessageService extends MappMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+    }
+}
