fe1936750578f5c653cfbda7b377bdebf688f790
Semsudin Tafilovic <sekitafilovic@gmail.com>
2021-08-27 09:29:06 +0200

    Added FcmRender as single point for firebase messages manipulation

--- src/main/java/com/appoxee/testapp/fcm/MessageService.java
+++ src/main/java/com/appoxee/testapp/fcm/MessageService.java
@@ -3,12 +3,15 @@ package com.appoxee.testapp.fcm;
 
 
 
+import androidx.annotation.NonNull;
+
 import com.appoxee.push.fcm.MappMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
 public  class MessageService extends MappMessagingService {
 
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
     }
