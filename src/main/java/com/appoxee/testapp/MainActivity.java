180b064abb04c37b8e4a8a976671f734f0306d0e
Semsudin Tafilovic <sekitafilovic@gmail.com>
2021-09-27 15:31:10 +0200

    Updated SDK version: 6.0.17-special8

--- src/main/java/com/appoxee/testapp/MainActivity.java
+++ src/main/java/com/appoxee/testapp/MainActivity.java
@@ -284,7 +284,7 @@ public class MainActivity extends Activity implements Appoxee.OnInitCompletedLis
         findViewById(R.id.dmcCallInApp).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Appoxee.instance().triggerDMCCallInApp(MainActivity.this, "app_open");
+                Appoxee.instance().triggerInApp(MainActivity.this, "app_open");
             }
         });
 
