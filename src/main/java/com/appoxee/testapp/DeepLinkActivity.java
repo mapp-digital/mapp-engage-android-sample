96abb1c930faa145c708d1af2712abd43d619634
semsudin.tafilovic <sekitafilovic@gmail.com>
2022-01-05 17:33:54 +0100

    Changed writing engage config into shared prefs

--- src/main/java/com/appoxee/testapp/DeepLinkActivity.java
+++ src/main/java/com/appoxee/testapp/DeepLinkActivity.java
@@ -21,9 +21,6 @@ public class DeepLinkActivity extends Activity {
 
     TextView tv;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
